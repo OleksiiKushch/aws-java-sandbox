4f6acb8a2493353981f2ce65a7fbda9530c0fdea
OleksiiKushch <oleksii.kushch@nure.ua>
2024-01-28 14:32:41 +0200

    task08

--- task08/jsrc/main/java/com/my/meteo/OpenMeteoApi.java
+++
@@ -1,8 +0,0 @@
-package com.my.meteo;
-
-public class OpenMeteoApi {
-
-    public String getLatestWeatherForecast() {
-        return "{\"id\": 2643743, \"name\": \"London\", \"cord\": {\"lon\": -0.13, \"lat\": 51.51}, \"main\": {\"temp\": 280.32, \"pressure\": 1012, \"humidity\": 81}, \"wind\": {\"speed\": 4.1, \"deg\": 80}, \"precipitation\": {\"1h\": 0.1}, \"clouds\": {\"all\": 90}}";
-    }
-}
